d41a5b90fa4891d597df6ef17aaa967a0b54bdd0
HWXLR8 <68k@segv.moe>
2025-10-31 15:27:37 -0400

    process LEDs on core1

--- src/main.c
+++ src/main.c
@@ -1,4 +1,6 @@
 #include "pico/stdlib.h"
+#include "pico/multicore.h"
+#include "pico/util/queue.h"
 #include "pico/bootrom.h"
 #include "hardware/gpio.h"
 #include "hardware/pio.h"
@@ -14,22 +16,22 @@ bool LEDS_ON = true;
 static const btn_cfg BTN_CFG[] = {
   /*
    pin    button   idle RGB   press RGB   debounce time */
-  {5,     LEFT,    5, 0, 5,   5, 0, 5,    KEY_DEBOUNCE},
-  {3,     DOWN,    5, 0, 5,   5, 0, 5,    KEY_DEBOUNCE},
-  {4,     RIGHT,   5, 0, 5,   5, 0, 5,    KEY_DEBOUNCE},
-  {2,     B1,      5, 0, 5,   5, 0, 5,    KEY_DEBOUNCE},
-  {10,    B2,      5, 0, 5,   5, 0, 5,    KEY_DEBOUNCE},
-  {11,    B3,      5, 0, 5,   5, 0, 5,    KEY_DEBOUNCE},
-  {12,    B4,      5, 0, 5,   5, 0, 5,    KEY_DEBOUNCE},
-  {13,    B5,      5, 0, 5,   5, 0, 5,    KEY_DEBOUNCE},
-  {6,     B6,      5, 0, 5,   5, 0, 5,    KEY_DEBOUNCE},
-  {7,     B7,      5, 0, 5,   5, 0, 5,    KEY_DEBOUNCE},
-  {8,     B8,      5, 0, 5,   5, 0, 5,    KEY_DEBOUNCE},
-  {9,     B9,      5, 0, 5,   5, 0, 5,    KEY_DEBOUNCE},
-  {27,    B10,     5, 0, 5,   5, 0, 5,    KEY_DEBOUNCE},
-  {18,    B11,     5, 0, 5,   5, 0, 5,    KEY_DEBOUNCE},
-  {19,    B12,     5, 0, 5,   5, 0, 5,    KEY_DEBOUNCE},
-  {26,    UP,      5, 0, 5,   5, 0, 5,    KEY_DEBOUNCE},
+  {5,     LEFT,    5, 0, 5,   100, 0, 100,    KEY_DEBOUNCE},
+  {3,     DOWN,    5, 0, 5,   100, 0, 100,    KEY_DEBOUNCE},
+  {4,     RIGHT,   5, 0, 5,   100, 0, 100,    KEY_DEBOUNCE},
+  {2,     B1,      5, 0, 5,   100, 0, 100,    KEY_DEBOUNCE},
+  {10,    B2,      5, 0, 5,   100, 0, 100,    KEY_DEBOUNCE},
+  {11,    B3,      5, 0, 5,   100, 0, 100,    KEY_DEBOUNCE},
+  {12,    B4,      5, 0, 5,   100, 0, 100,    KEY_DEBOUNCE},
+  {13,    B5,      5, 0, 5,   100, 0, 100,    KEY_DEBOUNCE},
+  {6,     B6,      5, 0, 5,   100, 0, 100,    KEY_DEBOUNCE},
+  {7,     B7,      5, 0, 5,   100, 0, 100,    KEY_DEBOUNCE},
+  {8,     B8,      5, 0, 5,   100, 0, 100,    KEY_DEBOUNCE},
+  {9,     B9,      5, 0, 5,   100, 0, 100,    KEY_DEBOUNCE},
+  {27,    B10,     5, 0, 5,   100, 0, 100,    KEY_DEBOUNCE},
+  {18,    B11,     5, 0, 5,   100, 0, 100,    KEY_DEBOUNCE},
+  {19,    B12,     5, 0, 5,   100, 0, 100,    KEY_DEBOUNCE},
+  {26,    UP,      5, 0, 5,   100, 0, 100,    KEY_DEBOUNCE},
   // no LEDs
   {14,    B13,     0, 0, 0,   0, 0, 0,    TAC_DEBOUNCE},
   {21,    B14,     0, 0, 0,   0, 0, 0,    TAC_DEBOUNCE},
@@ -46,54 +48,6 @@ void init_btns() {
     gpio_pull_up(p);
     gpio_set_dir(p, GPIO_IN);
   }
-}
-
-// reorder RGB -> GRB
-static inline uint32_t urgb_u32(uint8_t r, uint8_t g, uint8_t b) {
-    return ((uint32_t)(g) << 16) | ((uint32_t)(r) << 8) | (uint32_t)(b);
-}
-
-static inline void leds_off(PIO pio, uint sm) {
-  for (int i = 0; i < LED_BTN_COUNT; ++i) {
-    pio_sm_put_blocking(pio, sm, urgb_u32(0, 0, 0) << 8);
-  }
-  sleep_us(LATCH_TIME);
-}
-
-static inline void leds_on(PIO pio, uint sm) {
-  for (uint i = 0; i < LED_BTN_COUNT; ++i) {
-    uint r, g, b;
-    r = BTN_CFG[i].r;
-    g = BTN_CFG[i].g;
-    b = BTN_CFG[i].b;
-    pio_sm_put_blocking(pio, sm, urgb_u32(r, g, b) << 8);
-  }
-  sleep_us(LATCH_TIME);
-}
-
-static inline void illuminate_leds(PIO pio, uint sm, const bool pressed[]) {
-  uint r, g, b;
-  for (uint i = 0; i < LED_BTN_COUNT; ++i) {
-    r = BTN_CFG[i].r;
-    g = BTN_CFG[i].g;
-    b = BTN_CFG[i].b;
-    if (pressed[i]) {
-      r = BTN_CFG[i].rp;
-      g = BTN_CFG[i].gp;
-      b = BTN_CFG[i].bp;
-    }
-    pio_sm_put_blocking(pio, sm, urgb_u32(r, g, b) << 8);
-  }
-  sleep_us(LATCH_TIME);
-}
-
-static inline void toggle_leds(PIO pio, uint sm) {
-  if (LEDS_ON) {
-    leds_off(pio, sm);
-  } else {
-    leds_on(pio, sm);
-  }
-  LEDS_ON = !LEDS_ON;
 }
 
 typedef struct __attribute__((packed)) {
@@ -124,70 +78,53 @@ static inline bool is_pressed(uint pin, int idx) {
   return is_stable[idx];
 }
 
-static inline uint32_t read_buttons(btn_state* bstate, PIO pio, uint sm) {
-  uint32_t mask = 0;
-  static bool led_toggle_pressed = false;
-  bool pressed_led[LED_BTN_COUNT] = {0};
+typedef struct {
+  uint8_t rgb[LED_BTN_COUNT][3];
+} led_frame;
 
-  for (int i = 0; i < BTN_COUNT; ++i) {
-    const uint bit = BTN_CFG[i].bit;
-    const uint p   = BTN_CFG[i].pin;
-    bool pressed   = is_pressed(p, i);
+// core0 writes, core1 reads
+static queue_t ledq;
 
-    if (pressed) {
-      mask |= (uint32_t)(1u << bit);
+static void core1_main() {
+  // init LEDs
+  PIO pio = pio0;
+  uint sm = 0;
+  uint offset = pio_add_program(pio, &ws2812_program);
+  ws2812_program_init(pio, sm, offset, LED_PIN, 800000, false);
 
-      if (i < LED_BTN_COUNT) pressed_led[i] = true;
+  led_frame f;
+  while (true) {
+    queue_remove_blocking(&ledq, &f);
 
-      // timestamp when pressed
-      if (bit == LEFT)  { if (bstate->left  == 0) bstate->left  = ++bstate->time;}
-      if (bit == RIGHT) { if (bstate->right == 0) bstate->right = ++bstate->time;}
-      if (bit == UP)    { if (bstate->up    == 0) bstate->up    = ++bstate->time;}
-      if (bit == DOWN)  { if (bstate->down  == 0) bstate->down  = ++bstate->time;}
-
-      // LED toggle
-      if (p == LED_TOGGLE_PIN) {
-        if (!led_toggle_pressed) {
-          toggle_leds(pio, sm);
-        }
-        led_toggle_pressed = true;
-      }
-
-      // BOOTSEL
-      if (p == BOOTSEL_PIN) reset_usb_boot(0, 0);
-    } else {
-      if (bit == LEFT)  bstate->left  = 0;
-      if (bit == RIGHT) bstate->right = 0;
-      if (bit == UP)    bstate->up    = 0;
-      if (bit == DOWN)  bstate->down  = 0;
-      if (p == LED_TOGGLE_PIN) led_toggle_pressed = false;
+    for (uint i = 0; i < LED_BTN_COUNT; ++i) {
+      // convert RGB -> GRB
+      uint32_t color = ((uint32_t)f.rgb[i][1] << 16)
+        | ((uint32_t)f.rgb[i][0] << 8)
+        | (uint32_t)f.rgb[i][2];
+      pio_sm_put_blocking(pio, sm , color << 8);
     }
+    sleep_us(LATCH_TIME);
   }
+}
 
-  // illuminate buttons which are pressed
-  if (LEDS_ON) illuminate_leds(pio, sm, pressed_led);
-
-  // SOCD cleaning
-  if (SOCD == LAST_INPUT) {
-    if (bstate->left && bstate->right) {
-      if (bstate->left < bstate->right) mask &= ~(1 << LEFT);
-      else mask &= ~(1 << RIGHT);
+static inline void build_led_frame(led_frame* out, const bool pressed[]) {
+  for (uint i = 0; i < LED_BTN_COUNT; ++i) {
+    uint8_t r = BTN_CFG[i].r;
+    uint8_t g = BTN_CFG[i].g;
+    uint8_t b = BTN_CFG[i].b;
+    if (pressed[i]) {
+      r = BTN_CFG[i].rp;
+      g = BTN_CFG[i].gp;
+      b = BTN_CFG[i].bp;
     }
-    if (bstate->up && bstate->down) {
-      if (bstate->up < bstate->down) mask &= ~(1 << UP);
-      else mask &= ~(1 << DOWN);
-    }
-  } else if (SOCD == NEUTRAL) {
-    if (bstate->left && bstate->right) {
-      mask &= ~(1 << LEFT);
-      mask &= ~(1 << RIGHT);
-    }
-    if (bstate->up && bstate->down) {
-      mask &= ~(1 << UP);
-      mask &= ~(1 << DOWN);
-    }
+    out->rgb[i][0] = r;
+    out->rgb[i][1] = g;
+    out->rgb[i][2] = b;
   }
-  return mask;
+}
+
+static inline bool same_frame(const led_frame* a, const led_frame* b) {
+  return memcmp(a, b, sizeof(*a)) == 0;
 }
 
 int main(void) {
@@ -195,24 +132,105 @@ int main(void) {
   tusb_init();
   init_btns();
 
-  // init LEDs
-  PIO pio = pio0;
-  uint sm = 0;
-  uint offset = pio_add_program(pio, &ws2812_program);
-  ws2812_program_init(pio, sm, offset, LED_PIN, 800000, false);
-  leds_off(pio, sm);
-  leds_on(pio,sm);
+  queue_init(&ledq, sizeof(led_frame), 4);
+  multicore_launch_core1(core1_main);
 
-  btn_state bstate = {0};
-  uint32_t prev_bmask = 0;
+  btn_state   bstate = {0};
+  uint32_t    prev_bmask = 0;
+  bool        pressed_led[LED_BTN_COUNT] = {0};
+  static bool led_toggle_held = false;
+  led_frame   last_sent = {0};
 
   while (true) {
     tud_task();
-    uint32_t bmask = read_buttons(&bstate, pio, sm);
+
+    // scan buttons, returns mask + leds pressed
+    memset(pressed_led, 0, sizeof(pressed_led));
+    uint32_t bmask = 0;
+
+    for (uint i = 0; i < BTN_COUNT; ++i) {
+      const uint bit = BTN_CFG[i].bit;
+      const uint p   = BTN_CFG[i].pin;
+      bool pressed = is_pressed(BTN_CFG[i].pin, i);
+
+      if (pressed) {
+        bmask |= (1u << BTN_CFG[i].bit);
+        if (i < LED_BTN_COUNT) pressed_led[i] = true;
+
+        // timestamp when pressed
+        if (bit == LEFT)  { if (bstate.left  == 0) bstate.left  = ++bstate.time;}
+        if (bit == RIGHT) { if (bstate.right == 0) bstate.right = ++bstate.time;}
+        if (bit == UP)    { if (bstate.up    == 0) bstate.up    = ++bstate.time;}
+        if (bit == DOWN)  { if (bstate.down  == 0) bstate.down  = ++bstate.time;}
+
+        // LED toggle
+        if (p == LED_TOGGLE_PIN) {
+          if (!led_toggle_held) {
+            LEDS_ON = !LEDS_ON;
+          }
+          led_toggle_held = true;
+        }
+
+        // BOOTSEL
+        if (p == BOOTSEL_PIN) reset_usb_boot(0, 0);
+
+      } else {
+        if (bit == LEFT)  bstate.left  = 0;
+        if (bit == RIGHT) bstate.right = 0;
+        if (bit == UP)    bstate.up    = 0;
+        if (bit == DOWN)  bstate.down  = 0;
+        if (p == LED_TOGGLE_PIN) led_toggle_held = false;
+      }
+    }
+    // SOCD cleaning
+    if (SOCD == LAST_INPUT) {
+      if (bstate.left && bstate.right) {
+        if (bstate.left < bstate.right) bmask &= ~(1 << LEFT);
+        else bmask &= ~(1 << RIGHT);
+      }
+      if (bstate.up && bstate.down) {
+        if (bstate.up < bstate.down) bmask &= ~(1 << UP);
+        else bmask &= ~(1 << DOWN);
+      }
+    } else if (SOCD == NEUTRAL) {
+      if (bstate.left && bstate.right) {
+        bmask &= ~(1 << LEFT);
+        bmask &= ~(1 << RIGHT);
+      }
+      if (bstate.up && bstate.down) {
+        bmask &= ~(1 << UP);
+        bmask &= ~(1 << DOWN);
+      }
+    }
+
+    // send HID report
     if (tud_hid_ready() && bmask != prev_bmask) {
       gamepad_report_t rpt = {.buttons = bmask};
       tud_hid_report(0, &rpt, sizeof(rpt));
       prev_bmask = bmask;
+    }
+
+    // enqueue LED frame only if LEDs changed
+    led_frame f;
+    if (LEDS_ON) {
+      build_led_frame(&f, pressed_led);
+    } else {
+      // disable LEDS
+      for (uint i = 0; i < LED_BTN_COUNT; ++i) {
+        f.rgb[i][0] = 0;
+        f.rgb[i][1] = 0;
+        f.rgb[i][2] = 0;
+      }
+    }
+    if (!same_frame(&f, &last_sent)) {
+      // try to enqueue
+      if (!queue_try_add(&ledq, &f)) {
+        led_frame scratch;
+        // free one slot
+        queue_try_remove(&ledq, &scratch);
+        queue_try_add(&ledq, &f);
+      }
+      last_sent = f;
     }
   }
   return 0;
